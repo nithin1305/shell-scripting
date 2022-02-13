b9407edf3431a6c59180c0a4288cccba1a2c6104
nithin1305 <nithin1305@hotmail.com>
2022-02-13 19:45:42 +0530

    updated

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -6,3 +6,21 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 echo "Install Mongodb"
 yum install -y mongodb-org &>>$LOG_FILE
 
+echo "Update Mongodb config file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
+
+echo "Start Database"
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
+
+echo "Download schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+
+echo "Extract schema"
+cd /tmp
+unzip -o mongodb.zip &>>$LOG_FILE
+
+echo "Load schema"
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
