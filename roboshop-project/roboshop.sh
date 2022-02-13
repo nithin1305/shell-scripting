ed684052f4dae51feacee87dd4c8d4712c1f86a6
nithin1305 <nithin1305@hotmail.com>
2022-02-13 18:00:21 +0530

    updated

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -4,5 +4,5 @@ if [ -f components/$1.sh ]; then
   bash components/$1.sh
 else
   echo -e "\e[1;31mWrong Input\e[0m"
-  echo -e "\e[1;33mAvailable Inputs - fronted|mongodb|catalogue|redis|user|cart|mysql|shipping|rabbitmq|payment|dispatch\e[om"
+  echo -e "\e[1;33mAvailable Inputs - fronted|mongodb|catalogue|redis|user|cart|mysql|shipping|rabbitmq|payment|dispatch\e[0m"
 fi
