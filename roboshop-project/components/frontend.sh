03396eb07d2b3356cdf0639212d156a3cdd76bb6
nithin1305 <nithin1305@hotmail.com>
2022-02-13 18:09:20 +0530

    Updating roboshop-project

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,1 +1,44 @@
-echo Frontend
+#The frontend is the service in RobotShop to serve the web content over Nginx.
+#
+#To Install Nginx.
+#
+#```
+## yum install nginx -y
+## systemctl enable nginx
+## systemctl start nginx
+#
+#```
+#
+#Let's download the HTDOCS content and deploy under the Nginx path.
+#
+#```
+## curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#
+#```
+#
+#Deploy in Nginx Default Location.
+#
+#```
+## cd /usr/share/nginx/html
+## rm -rf *
+## unzip /tmp/frontend.zip
+## mv frontend-main/* .
+## mv static/* .
+## rm -rf frontend-master README.md
+## mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#
+#```
+#
+#Finally restart the service once to effect the changes.
+#
+#```
+## systemctl restart nginx
+#
+#```
+rm -f /tmp/roboshop.log
+
+echo "Installing nginx"
+yum install nginx -y >>/tmp/roboshop.log
+
+echo "Download frontend repo"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip">>/tmp/roboshop.log
